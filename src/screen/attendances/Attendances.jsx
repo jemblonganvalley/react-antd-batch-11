62dc286e4f02d491edff997cf516d705a4a801f6
jemblonganvalley <jemblonganvalley@gmail.com>
2024-08-07 18:50:45 +0700

    update

--- src/screen/attendances/Attendances.jsx
+++ src/screen/attendances/Attendances.jsx
@@ -1,4 +1,4 @@
-import { Button, Popconfirm, Table } from 'antd'
+import { Button, Drawer, Form, Input, Popconfirm, Table } from 'antd'
 import React, { useEffect, useState } from 'react'
 import supabase from '../../supabase'
 import dayjs from 'dayjs'
@@ -20,6 +20,35 @@ const Attendances = () => {
     const rowSelection = {
         selectedRowKeys,
         onChange: onSelectChange
+    }
+
+    // drawer logic area
+    const [open, setOpen] = useState(false)
+    const [selectedId, setSelectedId] = useState(null)
+
+    function handleOpenDrawer() {
+        setOpen(true)
+    }
+
+    function handleCloseDrawer() {
+        setOpen(false)
+        setSelectedId(null)
+    }
+
+    function handleUpdateAttendances(e){
+        console.info(e)
+        setState(prev => prev = {...prev, isLoading : true})
+        supabase.from("attendances").update({
+            fullname : e.fullname,
+            phone : e.phone,
+            email : e.email,
+            address : e.address
+        })
+        .eq("id", e.id)
+        .then(res => {
+            setState(prev => prev = {...prev, isLoading : false, refresh : true})
+            handleCloseDrawer()
+        })
     }
 
     const column = [
@@ -60,7 +89,7 @@ const Attendances = () => {
             title: "action",
             dataIndex: "id",
             key: "action",
-            render: (e) => (
+            render: (e, record) => (
                 <div className='flex gap-1 items-center'>
                     <Popconfirm title='yakin delete ?' onConfirm={() => { singleDelete(e) }}>
                         <Button type='primary' size='small' danger icon={<AiOutlineDelete />}
@@ -69,7 +98,10 @@ const Attendances = () => {
                     </Popconfirm>
 
                     <Button type='primary' size='small' icon={<AiOutlineEdit />}
-                        shape='circle'
+                        shape='circle' onClick={() => {
+                            handleOpenDrawer()
+                            setSelectedId(record)
+                        }}
                     />
                 </div>
             )
@@ -118,7 +150,7 @@ const Attendances = () => {
     }
 
     useEffect(() => {
-        supabase.from('attendances').select("*")
+        supabase.from('attendances').select("*").order('id','asc')
             .then((res) => {
                 setData(res.data)
             })
@@ -130,6 +162,54 @@ const Attendances = () => {
 
     return (
         <div>
+
+            <Drawer
+                title='update data'
+                onClose={handleCloseDrawer}
+                open={open}
+                placement='bottom'
+            >
+                {selectedId?.fullname ? (
+                    <Form onFinish={ handleUpdateAttendances } layout='vertical'
+                        className='grid grid-cols-2 gap-4 max-w-[800px] mx-auto'
+                    >
+                        
+                        <Form.Item label="id" name={"id"} initialValue={selectedId?.id} hidden >
+                            <Input  />
+                        </Form.Item>
+
+                        <Form.Item label="fullname" name={"fullname"} initialValue={selectedId?.fullname} >
+                            <Input  />
+                        </Form.Item>
+
+                        <Form.Item label="email" name={"email"} initialValue={selectedId?.email} >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item label="phone" name={"phone"} initialValue={selectedId?.phone} >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item label="address" name={"address"} initialValue={selectedId?.address} >
+                            <Input />
+                        </Form.Item>
+
+                        <div>
+                            <Button type='ghost' htmlType='button' onClick={ handleCloseDrawer }>
+                                Cancel
+                            </Button>
+                            <Button type='primary' htmlType='submit' loading={state.isLoading} 
+                                disabled={state.isLoading}
+                            >
+                                Submit
+                            </Button>
+                        </div>
+
+                    </Form>
+                ) : null}
+
+            </Drawer>
+
             <h1>
                 Attendances Page
             </h1>
